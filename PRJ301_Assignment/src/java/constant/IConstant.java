e172582127ce42fb626b301c5d4818eb695536d2
TruongNT632 <truongnt.studynwork@gmail.com>
2025-06-20 20:40:33 +0700

    delete unused

--- PRJ301_Assignment/src/java/constant/IConstant.java
+++ PRJ301_Assignment/src/java/constant/IConstant.java
@@ -13,10 +13,7 @@ public class IConstant {
 
     private IConstant() {
     }
-    
-    public static String NAME_REGEX = "^[a-zA-Z ]+$";
-    public static String TEXT_REGEX = "^[A-Za-z0-9 ,\\.]+$";
-    public static String NUMBER_REGEX = "^\\d+$";
+
     public static String USERNAME_REGEX = "^[a-zA-Z0-9._-]{3,20}$";
     public static String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
 
